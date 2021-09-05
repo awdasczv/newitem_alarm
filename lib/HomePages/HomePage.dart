20066e4f6d06c06f6155c9d85655a7488dafa587
이원재 <ysm04038@gmail.com>
2021-09-06 02:47:05 +0900

    Add Banner, dependencies
    배너 추가,
    의존성 추가(page_view_indicators: ^2.0.0)
    
    무한스크롤배너를 해봤는데 최초에 왼쪽으로 가는건 실패함, 최초 PageController에 initialpage 값을 그냥 겁나 큰수를 하면 되긴 하는데 그러면 간지가 안나서 다른거 좀 생각해 봐야 할듯

--- lib/HomePages/HomePage.dart
+++ lib/HomePages/HomePage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:page_view_indicators/page_view_indicators.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,12 +8,96 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final _colorList1 = [
+    Colors.blue,
+    Colors.orange,
+    Colors.green,
+    Colors.pink,
+    Colors.red,
+    Colors.amber,
+    Colors.brown,
+    Colors.yellow,
+  ];
+  final _colorList2 = [Colors.teal, Colors.black87];
+
+  final _pageController = PageController();
+  final _currentPageNotifier = ValueNotifier<int>(0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomePage 이원재'),
-      ),
+      body: ListView(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 300,
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemBuilder: (BuildContext context, int index){
+                    return  Container(
+                      color: _colorList2[_currentPageNotifier.value%2],
+                      child: Center(
+                        child: FlutterLogo(
+                          textColor: _colorList1[index % _colorList1.length],
+                          size: 100,
+                          style: FlutterLogoStyle.stacked,
+                        ),
+                      ),
+                    );
+                  },
+                  onPageChanged: (int index){
+                    _currentPageNotifier.value = index % _colorList1.length;
+                  },
+                ),
+              ),
+              Positioned(
+                left: 0.0,
+                right: 0.0,
+                bottom: 0.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CirclePageIndicator(
+                    itemCount: _colorList1.length,
+                    currentPageNotifier: _currentPageNotifier,
+                  ),
+                ),
+              ),
+              Positioned(
+                  left: 0.0,
+                  right: 0.0,
+                  top: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(onPressed: (){},child: Text('먹어봤니',style: TextStyle(fontSize: 20,color: Colors.white)),),
+                        IconButton(icon: Icon(Icons.search), onPressed: (){},iconSize: 30,color: Colors.white,)
+                      ],
+                    ),
+                  )
+              )
+            ],
+          ),
+          Padding(
+            padding:  EdgeInsets.all(8.0),
+            child: Text('카테고리', style: TextStyle(fontSize: 20),),
+          ),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  IconButton(icon: Icon(Icons.lunch_dining), onPressed: (){})
+                ]
+              )
+            ],
+          )
+        ],
+      )
     );
   }
 }
