cf4c39e6c7dbf900046e358b3caacd513920d98e
이원재 <ysm04038@gmail.com>
2021-09-06 07:43:25 +0900

    HomePage에 카테고리 추가
    버튼 _customButton 간단한건데 저거 구현하는데 은근 오래걸림 ㅡ.ㅡ

--- lib/HomePages/HomePage.dart
+++ lib/HomePages/HomePage.dart
@@ -31,76 +31,158 @@ class _HomePageState extends State<HomePage> {
         shrinkWrap: true,
         scrollDirection: Axis.vertical,
         children: [
-          Stack(
-            children: [
-              Container(
-                height: 300,
-                child: PageView.builder(
-                  controller: _pageController,
-                  itemBuilder: (BuildContext context, int index){
-                    return  Container(
-                      color: _colorList2[_currentPageNotifier.value%2],
-                      child: Center(
-                        child: FlutterLogo(
-                          textColor: _colorList1[index % _colorList1.length],
-                          size: 100,
-                          style: FlutterLogoStyle.stacked,
-                        ),
-                      ),
-                    );
-                  },
-                  onPageChanged: (int index){
-                    _currentPageNotifier.value = index % _colorList1.length;
-                  },
-                ),
-              ),
-              Positioned(
-                left: 0.0,
-                right: 0.0,
-                bottom: 0.0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CirclePageIndicator(
-                    itemCount: _colorList1.length,
-                    currentPageNotifier: _currentPageNotifier,
-                  ),
-                ),
-              ),
-              Positioned(
-                  left: 0.0,
-                  right: 0.0,
-                  top: 0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(onPressed: (){},child: Text('먹어봤니',style: TextStyle(fontSize: 20,color: Colors.white)),),
-                        IconButton(icon: Icon(Icons.search), onPressed: (){},iconSize: 30,color: Colors.white,)
-                      ],
-                    ),
-                  )
-              )
-            ],
-          ),
+          _banner(),
+          _category(),
           Padding(
-            padding:  EdgeInsets.all(8.0),
-            child: Text('카테고리', style: TextStyle(fontSize: 20),),
+            padding:  EdgeInsets.all(10),
+            child: Text('이주의 신상', style: TextStyle(fontSize: 20),),
           ),
-          Table(
-            children: [
-              TableRow(
-                children: [
-                  IconButton(icon: Icon(Icons.lunch_dining), onPressed: (){})
-                ]
-              )
-            ],
-          )
         ],
       )
     );
   }
+
+  Widget _banner(){
+    return Stack(
+      children: [
+        Container(
+          height: 300,
+          child: PageView.builder(
+            controller: _pageController,
+            itemBuilder: (BuildContext context, int index){
+              return  Container(
+                color: _colorList2[_currentPageNotifier.value%2],
+                child: Center(
+                  child: FlutterLogo(
+                    textColor: _colorList1[index % _colorList1.length],
+                    size: 100,
+                    style: FlutterLogoStyle.stacked,
+                  ),
+                ),
+              );
+            },
+            onPageChanged: (int index){
+              _currentPageNotifier.value = index % _colorList1.length;
+            },
+          ),
+        ),
+        Positioned(
+          left: 0.0,
+          right: 0.0,
+          bottom: 0.0,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CirclePageIndicator(
+              itemCount: _colorList1.length,
+              currentPageNotifier: _currentPageNotifier,
+            ),
+          ),
+        ),
+        Positioned(
+            left: 0.0,
+            right: 0.0,
+            top: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(onPressed: (){},child: Text('먹어봤니',style: TextStyle(fontSize: 20,color: Colors.white)),),
+                  IconButton(icon: Icon(Icons.search), onPressed: (){},iconSize: 30,color: Colors.white,)
+                ],
+              ),
+            )
+        )
+      ],
+    );
+  }
+
+  Widget _category(){
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding:  EdgeInsets.all(10),
+            child: Text('카테고리', style: TextStyle(fontSize: 20),),
+          ),
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _customButton(Icons.lunch_dining, '패스트푸드1'),
+                _customButton(Icons.lunch_dining, '패스트푸드2'),
+                _customButton(Icons.lunch_dining, '패스트푸드3'),
+                _customButton(Icons.lunch_dining, '패스트푸드4'),
+                _customButton(Icons.lunch_dining, '패스트푸드5'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _customButton(Icons.lunch_dining, '패스트푸드6'),
+                _customButton(Icons.lunch_dining, '패스트푸드7'),
+                _customButton(Icons.lunch_dining, '패스트푸드8'),
+                _customButton(Icons.lunch_dining, '패스트푸드9'),
+                _customButton(Icons.lunch_dining, '패스트푸드10'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _customButton(Icons.lunch_dining, '패스트푸드11'),
+                _customButton(Icons.lunch_dining, '패스트푸드12'),
+                _customButton(Icons.lunch_dining, '패스트푸드13'),
+                _customButton(Icons.lunch_dining, '패스트푸드14'),
+                _customButton(Icons.lunch_dining, '패스트푸드15'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _customButton(Icons.lunch_dining, '패스트푸드16'),
+                _customButton(Icons.lunch_dining, '패스트푸드17'),
+                _customButton(Icons.lunch_dining, '패스트푸드18'),
+                _customButton(Icons.lunch_dining, '패스트푸드19'),
+                _customButton(Icons.lunch_dining, '패스트푸드20'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _customButton(IconData icons, String label){
+    return Container(
+      width: 60,
+      child: InkWell(
+        //splashColor: Colors.green,
+        onTap: (){print(label);},
+        child: Column(
+          children: [
+            Icon(icons,size: 40,),
+            Text(label, style: TextStyle(fontSize: 10),),
+          ],
+        ) ,
+      ),
+    );
+  }
+
+
 }
+
 
 class TestProvider extends ChangeNotifier{
   void testFunction(){
