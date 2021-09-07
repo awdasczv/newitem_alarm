4db1fdfb3087dd576cd889f699fb922931b56170
dy-dev <ehdbs5525@naver.com>
2021-09-07 14:37:16 +0900

    new profile

--- lib/ProfilePages/ProfileHome.dart
+++ lib/ProfilePages/ProfileHome.dart
@@ -9,9 +9,40 @@ class _ProfileHomeState extends State<ProfileHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ProfilePage wonjaebabo'),
+      appBar: AppBar(
+        centerTitle: true,
+        titleSpacing: 0.0,
+        title: Text("My Page"),
       ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+                color: Colors.blue,
+                child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 400, top: 20, bottom: 20),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage('images/profile.png'),
+                  ),
+                )
+            ),
+          ),
+          // Expanded(
+          //     child: Container(
+          //         child: Column(
+          //             mainAxisAlignment: MainAxisAlignment.center,
+          //             children: <Widget>[
+          //               Text("Name"),
+          //               Text("date")
+          //             ]
+          //         )
+          //     )
+          // )
+        ]
+      )
     );
   }
 }
