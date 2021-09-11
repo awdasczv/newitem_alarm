3cbdd20f430566d3802d50f3f9710d6e26e7bd04
kimjuii <cathy033077@gmail.com>
2021-09-11 16:43:00 +0900

    탭바 기본 생성(상품, 워치) #DefaultTabController

--- lib/LikePages/LikeHome.dart
+++ lib/LikePages/LikeHome.dart
@@ -6,12 +6,34 @@ class LikeHome extends StatefulWidget {
 }
 
 class _LikeHomeState extends State<LikeHome> {
+  final bar = ["상품", "워치"];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('LikePage(kimjuii)'),
-      ),
-    );
+    return DefaultTabController(
+        length: bar.length,
+        child: MaterialApp(
+            home: Scaffold(
+          appBar: AppBar(
+            title: Text("찜목록"),
+            bottom: TabBar(
+              onTap: (index) {},
+              tabs: bar.map((String choice) {
+                return Tab(
+                  text: choice,
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Container(
+                color: Colors.yellow,
+              ),
+              Container(
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        )));
   }
 }
