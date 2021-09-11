d36f0499da9f7f7585c614a701dd124957540c93
dy-dev <ehdbs5525@naver.com>
2021-09-11 15:53:41 +0900

    2021-09-11 update profile

--- lib/ProfilePages/ProfileHome.dart
+++ lib/ProfilePages/ProfileHome.dart
@@ -9,31 +9,48 @@ class _ProfileHomeState extends State<ProfileHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+        /*appBar: AppBar(
           centerTitle: true,
           titleSpacing: 0.0,
           title: Text("My Page"),
-        ),
+        ),*/
         body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
+                padding: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                color: Colors.blue,
+                child: Center(
+                  child: Text("My Page",
+                    style: TextStyle(fontSize: 20),
+                    //textAlign: TextAlign.center,
+                  ),
+                )
+              ),
+              Container(
                   color: Colors.blue,
-                  padding: EdgeInsets.only(left: 10, right: 10, top: 50, bottom: 50),
+                  padding: EdgeInsets.only(left: 10, right: 10, top: 40, bottom: 80),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Colors.grey,
-                        backgroundImage: AssetImage('assets/images/profile3.png'),
+                      Container(
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(15),
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundColor: Colors.white60,
+                          backgroundImage: AssetImage('assets/images/profile3.png'),
+                        ),
                       ),
                       Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Text("  Name"),
-                          Text(" "),
-                          Text("  Date")
+                          Text("Name",
+                              style: TextStyle(fontSize: 18),
+                              textAlign: TextAlign.left),
+                          Text("Date",
+                              style: TextStyle(fontSize: 18),
+                              textAlign: TextAlign.left)
                         ],
                       )
 
