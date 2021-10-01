0b63dc8c5e1b0175e4a6d027a60ea855d26af18e
dy-dev <89628091+doyoon-dev@users.noreply.github.com>
2021-10-01 19:52:17 +0900

    new

--- lib/ProfilePages/CommandMan.dart
+++ lib/ProfilePages/CommandMan.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
+class CommandMan extends StatefulWidget {
+  @override
+  _CommandManState createState() => _CommandManState();
+}
+
+class _CommandManState extends State<CommandMan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("댓글 관리",
+        style: TextStyle(fontSize: 20),),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios)
+        ),
+      ),
+    );
+  }
+
+}
+
+/*
 class CommandMan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,4 +38,4 @@ class CommandMan extends StatelessWidget {
       ),
     );
   }
-}
+}*/
