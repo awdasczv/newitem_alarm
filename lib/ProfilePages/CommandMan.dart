3ae116f99aff483fb23b74ee597b1f193b226188
dy-dev <89628091+doyoon-dev@users.noreply.github.com>
2021-10-07 14:30:50 +0900

    newcomment

--- lib/ProfilePages/CommandMan.dart
+++ lib/ProfilePages/CommandMan.dart
@@ -22,16 +22,11 @@ class _CommandManState extends State<CommandMan> {
   var _isChecked4 = false;
   var _isChecked5 = false;
 
-  bool visible = false;
+  bool editButton = false;
+  bool deleteButton = false;
 
-  void _delete() {
-    setState(() {
-      Text(" 삭제");
-      if(_isChecked1 = true) {
+  int index;
 
-      }
-    });
-  }
 
 
   /*void edit() {
@@ -81,11 +76,11 @@ class _CommandManState extends State<CommandMan> {
                         onPressed: () {
                           Text(" 편집");
                           setState(() {
-                            visible = true;
+                            editButton = true;
                           });
                         },
                         // 편집을 누르면 편집 버튼이 삭제로 변경
-                        child: visible == true ? Text(" 삭제") : Text(" 편집")
+                        child: editButton == true ? _delete() : Text(" 편집")
                     )
                 )
               ],
@@ -261,11 +256,63 @@ class _CommandManState extends State<CommandMan> {
       padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
       itemCount: _comment.length,
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(_comment[index], style: TextStyle( fontSize: 23),),
-        );
+        if( editButton == false ) {
+          return ListTile(
+            title: Text(_comment[0], style: TextStyle(fontSize: 23),),
+            onTap:() {},
+          );
+        }
+        else return _delete();
+        /*Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: Text(_comment[0], style: TextStyle(fontSize: 23),),
+                  onTap:() {},
+                ),
+                /*editButton == true ? Checkbox(
+                  value: _isChecked1,
+                  onChanged: (value) {
+                    setState(() {
+                      _isChecked1 = value;
+                    });
+                  },
+                ) : Text(""),*/
+              ],
+            )
+          ],
+        );*/
       },
       separatorBuilder: (BuildContext context, int index) => Divider(color: Colors.black26,thickness: 2,)
     );
+  }
+  Widget _delete() {
+    setState(() {
+      Text(" 삭제");
+      Checkbox(
+        value: _isChecked1,
+        onChanged:  (value) {
+          setState(() {
+            _isChecked1 = value;
+          });
+        },
+      );
+      setState(() {
+        if(_isChecked1 = true) {
+          Dismissible(
+            key: Key(_comment[0]),
+            onDismissed: (direction) {
+              setState(() {
+                _comment.removeAt(0);
+              });
+            },
+          );
+        }
+      });
+    });
+    return _delete();
   }
 }
