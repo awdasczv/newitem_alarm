434a0440ff4311f17fdc37d01cc72f018832a7f7
kimjuii <cathy033077@gmail.com>
2021-11-07 08:56:07 +0900

    상품상세보기화면(goodsDetatil) 생성 & 상품상세보기화면의 Body부분 위젯 생성 & 상품 데이터 모델(goods) 생성

---
+++ lib/LikePages/GoodsPages/goodsDetail.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../LikePages/GoodsPages/goodsDetail_Body.dart';
+import '../../model/goods.dart';
+
+class DetailMain extends StatefulWidget {
+  final Goods goods; //goods.dart에 있는 Goods 객체 넘겨받기
+  const DetailMain({Key key, @required this.goods}) : super(key: key);
+
+  @override
+  _DetailMainState createState() => _DetailMainState();
+}
+
+class _DetailMainState extends State<DetailMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.black),
+          backgroundColor: Colors.white,
+        ),
+        body: Body(goods: goodsList[0]));
+  }
+}
