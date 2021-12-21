ca53b9b33955c4384332ccdef2e77edc5748dfb2
dy-dev <89628091+doyoon-dev@users.noreply.github.com>
2021-12-21 17:37:39 +0900

    2021-12-21-dy

--- lib/ProfilePages/ProfileHome.dart
+++ lib/ProfilePages/ProfileHome.dart
@@ -24,7 +24,18 @@ class _ProfileHomeState extends State<ProfileHome> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: func(_isLogin));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          '프로필',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 25, color: Colors.black),
+        ),
+      ),
+        body: func(_isLogin)
+    );
   }
 
   // 로그인/회원가입, 프로필 페이지
@@ -33,19 +44,7 @@ class _ProfileHomeState extends State<ProfileHome> {
       return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-                padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                color: Colors.orangeAccent,
-                child: Center(
-                  child: Text(
-                    "My Page",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        color: Colors.white),
-                    //textAlign: TextAlign.center,
-                  ),
-                )),
+
             LoginProfile(),
             ProfileImage(),
             ListMenu()
@@ -59,7 +58,7 @@ class _ProfileHomeState extends State<ProfileHome> {
   // 프로필
   Widget LoginProfile() {
     return Container(
-        color: Colors.orangeAccent,
+        color: Colors.transparent,
         padding: EdgeInsets.only(left: 10, right: 10, top: 15, bottom: 15),
         child: Center(
           child: Column(
@@ -67,7 +66,7 @@ class _ProfileHomeState extends State<ProfileHome> {
             children: [
               //프로필 사진
               Container(
-                color: Colors.orangeAccent,
+                color: Colors.transparent,
                 padding:
                     EdgeInsets.only(left: 20, right: 30, top: 20, bottom: 20),
                 child: CircleAvatar(
@@ -84,7 +83,7 @@ class _ProfileHomeState extends State<ProfileHome> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(_name,
-                      style: TextStyle(fontSize: 30, color: Colors.white),
+                      style: TextStyle(fontSize: 30,),
                       textAlign: TextAlign.center),
                 ],
               )
@@ -97,21 +96,29 @@ class _ProfileHomeState extends State<ProfileHome> {
   Widget ProfileImage() {
     return GestureDetector(
         child: Container(
-            color: Colors.orangeAccent,
-            padding: EdgeInsets.fromLTRB(0, 7, 0, 7),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.edit,
-                  color: Colors.white,
-                ),
-                Text(
-                  " 프로필 수정하기",
-                  style: TextStyle(fontSize: 25, color: Colors.white),
-                )
-              ],
-            )),
+          height: 70,
+          child: Card(
+
+            // decoration: BoxDecoration(
+            //   border: Border.all(width: 1),
+            //   color: Colors.transparent,
+            // ),
+
+            //padding: EdgeInsets.fromLTRB(0, 7, 0, 7),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    " 프로필 수정하기",
+                    style: TextStyle(fontSize: 25, color: Colors.black),
+                  )
+                ],
+              )),
+        ),
         onTap: () async {
           var a = await Navigator.push(
             context,
