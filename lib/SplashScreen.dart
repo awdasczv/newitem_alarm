221409af102d47389c36ce03724de8e9c003c180
HIBINGSU <89457852+HIBINGSU@users.noreply.github.com>
2021-12-27 19:43:25 +0900

    splashscreen

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 
-
 class SplashScreen extends StatelessWidget {
   static String routeName = "/splash";
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          "먹어봤니?",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 59.0,
-                fontWeight: FontWeight.bold,
-        ),
-        ),
-      )
-    );
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: FittedBox(
+              child: Image.asset('assets/images/s.png'),
+              fit: BoxFit.cover,
+            )
+        ));
   }
 }
