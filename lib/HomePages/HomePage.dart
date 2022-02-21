d27d66f3fd8da5acce2c3b634d3aa81c5d5b0f54
이원재 <ysm04038@gmail.com>
2022-02-21 15:24:08 +0900

    2월 21일 14시 21분 커밋

--- lib/HomePages/HomePage.dart
+++ lib/HomePages/HomePage.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:newitem_alarm/GoodsPages/goodsDetail.dart';
 import 'package:newitem_alarm/HomePages/FastFood.dart';
 import 'package:newitem_alarm/HomePages/SearchPage.dart';
 import 'package:newitem_alarm/model/Favorite_button.dart';
+import 'package:newitem_alarm/model/Firestore_model.dart';
 import 'package:page_view_indicators/page_view_indicators.dart';
 
 import '../GoodsPages/goodsDetail_New.dart';
@@ -22,6 +24,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  CollectionReference goodsRef = FirebaseFirestore.instance.collection('Goods');
+  
+  Widget _newProductList(){
+    return FutureBuilder<QuerySnapshot>(
+      future:  goodsRef.orderBy('launchdate',descending: false).get(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Color(0xfff1c40f))),
+          );
+        } else if (snapshot.hasError) {
+          return Center(child: Text(snapshot.error.toString()));
+        }
+        final List<QueryDocumentSnapshot<Object>> _newGoodsList = snapshot.data.docs;
+        print(_newGoodsList.length);
+        return ListView.builder(
+            itemCount: _newGoodsList.length - 1,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return _itemContainer(_newGoodsList[index].data(), index);
+            });
+      });
+  }
+
   final _colorList1 = [
     Color(0xffaee4ff),
     Color(0xffffe4af),
@@ -35,7 +64,7 @@ class _HomePageState extends State<HomePage> {
 
   List<bool> _isfavorite;
   final _colorList2 = [Colors.black54, Colors.black87];
-
+  
   @override
   void initState() {
     // TODO: implement initState
@@ -65,7 +94,7 @@ class _HomePageState extends State<HomePage> {
                 style: TextStyle(fontSize: 20),
               ),
             ),
-            _itemList()
+            _newProductList()
           ],
         ));
   }
@@ -232,7 +261,8 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
+  
+/*
   Widget _itemList() {
     return ListView.builder(
         shrinkWrap: true,
@@ -241,9 +271,11 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (BuildContext context, int index) {
           return _itemContainer(index);
         });
-  }
+  }*/
 
-  Widget _itemContainer(int index) {
+  Widget _itemContainer(var _item,index) {
+
+    NewGoods _goods = NewGoods.fromJson(_item);
     Icon _icon() {
       if (_isfavorite[index]) {
         return Icon(
@@ -284,7 +316,7 @@ class _HomePageState extends State<HomePage> {
                                     image: DecorationImage(
                                         fit: BoxFit.contain,
                                         image: NetworkImage(
-                                            goodsList[index].imageUrl[0]))),
+                                            _goods.imageURL[0]))),
                               ),
                               Positioned(
                                 right: 8,
@@ -322,11 +354,11 @@ class _HomePageState extends State<HomePage> {
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
                                     Text(
-                                      goodsList[index].title,
+                                      _goods.title,
                                       style: TextStyle(fontSize: 17),
                                     ),
                                     Text(
-                                      goodsList[index].price.toString() + "원",
+                                      _goods.price.toString() + "원",
                                       style: TextStyle(fontSize: 17),
                                     )
                                   ],
