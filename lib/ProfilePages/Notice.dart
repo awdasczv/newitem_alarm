66ebd35802c226da7e88ab7a59292199e8def0af
HIBINGSU <ksw4093@naver.com>
2022-05-21 14:40:19 +0900

    오류

--- lib/ProfilePages/Notice.dart
+++ lib/ProfilePages/Notice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:newitem_alarm/ProfilePages/NoticeList.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -16,30 +17,69 @@ class Notice extends StatelessWidget {
               child: CircularProgressIndicator(
                   valueColor: AlwaysStoppedAnimation(Color(0xffFFC845))),
             );
-          }else if (snapshot.hasError) {
+          }
+          else if (snapshot.hasError) {
             return Center(child: Text(snapshot.error.toString()));
           }
           else {
-
             List<dynamic> snapshotList = snapshot.data.docs.map(
-                    (DocumentSnapshot snapshot)
+                    (DocumentSnapshot document)
                 {
-                  Timestamp tt = snapshot["datetime"];
-                  DateTime dt = DateTime.fromMicrosecondsSinceEpoch(tt.microsecondsSinceEpoch);
+                  Timestamp tt = document["datetime"];
+                  DateTime now = DateTime.now();
+                  String formattedDate = DateFormat('yyyy-MM-dd').format(now);
                   return Padding(
                     padding: const EdgeInsets.all(3.0),
-                    child: Container(
-                      height: 200,
-                      color: Colors.grey,
-                      child: Text(snapshot["Title"]),
+                    child: GestureDetector(
+                      child: Card(
+                        elevation: 2,
+                        child: Container(
+                          height: 100,
+                          padding: const EdgeInsets.all(8),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(document["Title"],
+                                    style:TextStyle(
+                                      color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(formattedDate.toString(),
+                                  style: TextStyle(color:  Colors.grey[600]),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 8,),
+                              Container(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  document["description"],
+                                  maxLines: 3,
+                                  style: TextStyle(color: Colors.black54),
+
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      onTap: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => NoticeList()),
+                        );
+                      },
                     ),
                   );
                 }).toList();
-
             List<Widget> noticeItem = snapshotList.cast<Widget>();
 
             return CustomScrollView(
               // CustomScrollView는 children이 아닌 slivers를 사용하며, slivers에는 스크롤이 가능한 위젯이나 리스트가 등록가능함
+              // CustomScrollView 아래에는
               slivers: <Widget>[
                 // 앱바 추가
                 SliverAppBar(
@@ -80,55 +120,5 @@ class Notice extends StatelessWidget {
   }
 }
 
-/*
-CustomScrollView(
-        // CustomScrollView는 children이 아닌 slivers를 사용하며, slivers에는 스크롤이 가능한 위젯이나 리스트가 등록가능함
-        slivers: <Widget>[
-          // 앱바 추가
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            centerTitle: true,
-            title : Container(
-              child: Text("공지사항",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 25, color: Colors.black),
-            ),
-            ),
 
-            // floating 설정. SliverAppBar는 스크롤 다운되면 화면 위로 사라짐.
-            // true: 스크롤 업 하면 앱바가 바로 나타남. false: 리스트 최 상단에서 스크롤 업 할 때에만 앱바가 나타남
-            leading: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(Icons.arrow_back, color: Colors.black)
-            ),
-            floating: true,
-            snap: true,
-            // 최대 높이
-            expandedHeight: 50,
-          ),
-          SliverList(
-            // 아이템을 빌드하기 위해서 delegate 항목을 구성함
-            // SliverChildBuilderDelegate는 ListView.builder 처럼 리스트의 아이템을 생성해줌
-            // Padding으로 박스형식으로 끝부분 만들어줌
-              delegate:SliverChildListDelegate(
-                  snapshot.data.docs.map(
-                          (DocumentSnapshot snapshot)
-                      {
-                        Timestamp tt = snapshot["datetime"];
-                        DateTime dt = DateTime.fromMicrosecondsSinceEpoch(tt.microsecondsSinceEpoch);
-                        return Padding(
-                          padding: const EdgeInsets.all(3.0),
-                          child: Container(
-                            height: 200,
-                            color: Colors.grey,
-                            child: Text(snapshot["Title"]),
-                          ),
-                        );
-                      }).toList()
-              )
-          )
-        ],
-      ),
-*/
+
--- lib/ProfilePages/NoticeList.dart
+++ lib/ProfilePages/NoticeList.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:newitem_alarm/ProfilePages/NoticeList_body.dart';
-import 'package:newitem_alarm/ProfilePages/NoticeList_header.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class NoticeList extends StatelessWidget {
+  CollectionReference noticeRef = FirebaseFirestore.instance.collection("Notice");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,14 +23,12 @@ class NoticeList extends StatelessWidget {
             icon: Icon(Icons.arrow_back, color: Colors.black)
         ),
       ),
-      body: ListView(
+      body:ListView(
         children: [
-          NoticeList_header(),
-          //NoticeList_header 값을 받음
-      Padding(
-        padding: const EdgeInsets.only(bottom: 1.0),
-          child:NoticeList_body(), //NoticeList_body 값을 받음
-      ),
+          NoticeHeader(),
+          Padding(padding: const EdgeInsets.only(bottom: 1.0),
+            child:NoticeBody(),
+          )
         ],
       ),
     );
@@ -37,3 +36,62 @@ class NoticeList extends StatelessWidget {
 }
 
 
+Widget NoticeHeader() {
+  CollectionReference noticeRef = FirebaseFirestore.instance.collection("Notice");
+  return Scaffold(
+    body: StreamBuilder(
+      stream: noticeRef.snapshots(),
+      builder: (context, snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Center(
+            child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Color(0xffFFC845))),
+          );
+        }
+        else if (snapshot.hasError){
+          return Center(child: Text(snapshot.error.toString()));
+        }
+        else{
+          ListView(
+            children: snapshot.data.documents.map((DocumentSnapshot document)
+            {
+              Timestamp tt = document["datetime"];
+              DateTime now = DateTime.now();
+              String formattedDate = DateFormat('yyyy-MM-dd').format(now);
+              return Card(
+                margin: EdgeInsets.only(bottom: 12.0),
+                elevation: 0.5,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(document["Title"],
+                      style: TextStyle(
+                        color:  Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                      Text(formattedDate.toString(),
+                        style: TextStyle(color:  Colors.grey[600]),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }
+            ),
+          );
+        }
+      },
+    ),
+  );
+}
+
+
+
+
+
+Widget NoticeBody(){
+
+}
